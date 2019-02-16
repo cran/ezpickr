dca71c290105bbb9ea4ab2854ced7de1979f76db
JooYoung Seo <jooyoung@psu.edu>
2019-02-16 17:10:03 +0000

    version 1.0.1

--- R/view.R
+++ R/view.R
@@ -4,12 +4,11 @@
 #' @aliases view
 #' @keywords view
 
-#' @description You can use this function for loading any data.frame, data_frame, tbl_df, matrix, vector objects into your system-default spreadsheet software (e.g., Excel). This function has been inspired by \code{\link[BrailleR]{DataViewer}} and has implemented Hadley Wickham's \code{\link[readr]{write_csv}} instead of the default \code{\link[utils]{write.csv}} for a better performance.
+#' @description You can use this function for loading any data.frame, data_frame, tbl_df, matrix, table, vector objects into your system-default spreadsheet software (e.g., Excel). This function has been inspired by \code{\link[BrailleR]{DataViewer}} and has implemented \code{\link[writexl]{write_xlsx}} instead of the default \code{\link[utils]{write.csv}} for a better performance.
 
 #' @export view
-#' @param x An object of class data.frame, matrix, or vector.
-#' @param mode Character value for session locale and encoding; available values are: "ko1" for "CP949"; "ko2" for "UTF-8" while both change R locale into Korean (default is the current locale and encoding of your R session).
-#' @param ... Any additional arguments available for \link[readr]{write_csv}.
+#' @param x An object of class data.frame, matrix, table or vector.
+#' @param ... Any additional arguments available for \link[writexl]{write_xlsx}.
 
 #' @details
 #' See example below.
@@ -17,16 +16,19 @@
 #' @return Data object opened in a preferable spreadsheet application window.
 
 #' @examples
-
 #' if(interactive()) {
 #' library(ezpickr)
 #' data(airquality)
 #' str(airquality)
 
 #' ## View your data object in your spreadsheet software:
-#' # Please use additional argument: either `mode="ko1"` or `mode="ko2"` for Korean dataset.
 #' view(airquality)
 #' # Then, when necessary, you can modify the opened data in the spreadsheet and save it as a new data.
+#' 
+#' # You can pass a list object to the `view()` function like below:
+#' l <- list(iris = iris, mtcars = mtcars, chickwts = chickwts, quakes = quakes)
+#' view(l)
+#' # Then, each list item will appear in your Excel window sheet by sheet.
 #' }
 
 #' @author JooYoung Seo, \email{jooyoung@psu.edu}
@@ -34,46 +36,37 @@
 
 # Function starts:
 view <- 
-function(x, mode = NULL, ...) {
+function(x, ...) {
 
       # only for interactive sessions
   if (interactive()) {
-	if(!is.null(mode)) {
-		if(mode == "ko1") {
-			Sys.setlocale("LC_ALL", "Korean")
-			options(encoding="CP949")
-		} else if(mode == "ko2") {
-			Sys.setlocale("LC_ALL", "Korean")
-			options(encoding="UTF-8")
-		}
-	}
 
-    if(is.matrix(x)) {
+    if(is.matrix(x) || is.table(x)) {
       x <- data.frame(x)
     }
 
-    tmp <- tempfile(fileext = ".csv")
-    readr::write_csv(x, tmp, ...)
+    tmp <- tempfile(fileext = ".xlsx")
+    writexl::write_xlsx(x, tmp, ...)
     utils::browseURL(tmp)
 
     Sys.sleep(5)
     file <- readline("Enter the file name you want to save as (press enter to skip): ")
 
     if(file != "") {
-      if(!stringr::str_detect(file, "(.csv)$")) {
-        new_file <- paste0(file, ".csv")
+      if(!stringr::str_detect(file, "(.xlsx)$")) {
+        new_file <- paste0(file, ".xlsx")
       } else {
         new_file <- file
       }
 
       file.copy(from=tmp, to=paste0(getwd(), "/", new_file))
-    return(readr::read_csv(new_file))
+    return(readxl::read_excel(new_file))
   }
 
     file.remove(tmp)
 }  # end interactive
   else {
-    warning("This function is only useful in interactive sessions running under Windows.")
+    warning("This function is only useful in interactive sessions.")
 }  # end not interactive
       invisible(NULL)
 # Function ends.
--- inst/NEWS
+++ inst/NEWS
@@ -1,3 +1,13 @@
+# ezpickr 1.0.1
+
+* New function `picko()` has been added to assist Korean R users in importing Korean-included `path/to/file` as an alternative of `pick()` function; other non-Korean R users can still employ the existing `pick()` function.
+* A more robust mechanism has been applied to a file path that contains any Korean characters.
+* `pick()` and `picko()` functions now automatically return an Excel file that contains more than one sheet as a list of each Work Sheet (Thanks hyun seung Lee for the suggestion via email).
+* `view()` function now internally employs `writexl::write_xlsx()` function instead of `readr::write_csv()` to create Microsoft Excel file to provide users with a stable encoding consistency; since this change, `mode` option of the `view()` function has been removed.
+* `table` class is now supported in `view()` function by automatically converting it into data.frame class.
+* Startup message has been added to the package so that Korean R users can benefit from the newly developed `picko()` function when interacting with Korean-included dataset.
+
+
 # ezpickr 1.0.0
 
 * `mode` argument has been added to `pick()` function to accommodate Korean R users' needs in terms of R session locale and encoding to Korean. Available values include "ko1" for "CP949" and "ko2" for "UTF-8" while both change R locale into Korean.
@@ -9,10 +19,12 @@
 * Unused dependencies are moved to "suggest" from "import" field in the description file not to force unnecessary installation for users.
 * `interactive()` condition is added to the `pick()` example to avoid any error when called in a non-interactive testing environment.
 
+
 # ezpickr 0.1.2
 
 * NEWS file is added to help HTML page.
 * Authorship fixed for Soyoung Choi from contributor to author role for her considerable help for the `pick()` function.
+
 
 # ezpickr 0.1.1
 
